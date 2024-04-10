85244e1e15d0d1ae11d5840fbb958ec81ae6f417
Mahi Bhai <maheshlekhwani93@gmail.com>
2024-04-10 15:04:49 +0530

    express commit

---
+++ src/app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+const port = 3000;
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.listen(port, function(req, res) {
+    console.log(`Server listneing at port: ${port}`)
+})
